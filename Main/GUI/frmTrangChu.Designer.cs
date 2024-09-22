5051532ae2d71feb78319f953ae6675c8e0b0e26
kimphuongnt <kimphuongnt.dev@gmail.com>
2024-09-22 16:05:01 +0700

    Add project files.

---
+++ Main/GUI/frmTrangChu.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Main
+{
+    partial class frmTrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Header
+            // 
+            this.Header.Location = new System.Drawing.Point(306, 35);
+            this.Header.Size = new System.Drawing.Size(494, 57);
+            // 
+            // Aside
+            // 
+            this.Aside.Size = new System.Drawing.Size(306, 415);
+            // 
+            // frmTrangChu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmTrangChu";
+            this.Text = "Form1";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(22, 22, 800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Main/GUI/frmTrangChu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+namespace Main
+{
+    public partial class frmTrangChu : UIAsideHeaderMainFrame
+    {
+        public frmTrangChu()
+        {
+            InitializeComponent();
+            int pageIndex = 1000;
+            TreeNode root = Aside.CreateNode("QUẢN LÝ KHÁCH HÀNG", 61451, 24, pageIndex);
+
+            pageIndex = 2000;
+            root = Aside.CreateNode("QUẢN LÝ GIAO DỊCH", 61451, 24, pageIndex);
+            
+        }
+    }
+}
