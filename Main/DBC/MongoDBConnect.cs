c117cce81381bff833d4bc0c205027a1b0666139
kimphuongnt <kimphuongnt.dev@gmail.com>
2024-10-04 17:00:52 +0700

    fix MongoDBConnect

--- Main/DBC/MongoDBConnect.cs
+++ Main/DBC/MongoDBConnect.cs
@@ -13,6 +13,33 @@ namespace Main.DBC
     {
         private IMongoDatabase _database;
         public static string ConnectionString = "mongodb+srv://kimphuong8694:123@quanlykhachhang.khsds.mongodb.net/";
+        public static string DatabaseName = "QuanLyKhachHangThanThiet";
+
+        public IMongoDatabase Database
+        {
+            get { return _database; }
+            set { _database = value; }
+        }
+        public MongoDBConnect()
+        {
+            var client = new MongoClient(ConnectionString);
+            Database = client.GetDatabase(DatabaseName);
+        }
+        // Lấy tất cả dữ liệu từ một collection
+        public List<BsonDocument> GetAllDocuments(string collectionName)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        // Chèn một document vào collection
+        public void InsertDocument(string collectionName, BsonDocument document)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            collection.InsertOne(document);
+        }
+
+        // Cập nhật một document trong collection
         public UpdateResult UpdateDocument(string collectionName, FilterDefinition<BsonDocument> filter, UpdateDefinition<BsonDocument> update)
         {
             var collection = _database.GetCollection<BsonDocument>(collectionName);
@@ -69,7 +96,7 @@ namespace Main.DBC
 
         public List<BsonDocument> Search(string collectionName, string searchText, string searchField)
         {
-            var collection = _database.GetCollection<BsonDocument>(collectionName); 
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
             var filter = Builders<BsonDocument>.Filter.Regex(searchField, new BsonRegularExpression(searchText, "i"));
             return collection.Find(filter).ToList();
         }
